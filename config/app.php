a71509557ab8c534824948c62ef15a0af15d3817
Hugo Jeller <hugo_jeller@hotmail.com>
2017-12-25 00:19:57 -0300

    Modified cost per day

--- config/app.php
+++ config/app.php
@@ -249,7 +249,7 @@ return [
 
     'expiration_time_min' => 1,
 
-    'cost_per_day'   => 4,
+    'cost_per_day'   => 2,
     'sentry_enabled' => env('SENTRY_ENABLED'),
     'daemon_address' => env('DAEMON_ADDRESS'),
     'update_server'  => env('UPDATE_SERVER'),
