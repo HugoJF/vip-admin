d9b613885bda13495d0add58acbd8e5f5c3d4668
Hugo Jeller <hugo_jeller@hotmail.com>
2018-01-15 14:55:55 -0300

    Added log messages v2

--- app/Console/Commands/RefreshOPSkinsCache.php
+++ app/Console/Commands/RefreshOPSkinsCache.php
@@ -10,113 +10,118 @@ use Illuminate\Support\Facades\Log;
 
 class RefreshOPSkinsCache extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'opskins:refresh {--detailed}';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'opskins:refresh {--detailed}';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Refreshes the OPSkins IPricing API cache';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Refreshes the OPSkins IPricing API cache';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $this->info('Setting new memory limit.');
-        \Log::info('Setting new memory limit.');
-        ini_set('memory_limit', '512M');
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$this->info('Setting new memory limit.');
+		\Log::info('Setting new memory limit.');
+		ini_set('memory_limit', '512M');
 
-        $this->info('Downloading OPSkins information from CDN...');
-        \Log::info('Downloading OPSkins information from CDN...');
-        $inventory = Curl::to('https://api.opskins.com/IPricing/GetPriceList/v2/?appid=730')->asJson()->get();
-        $this->info('Received information from CDN!');
-        \Log::info('Received information from CDN!');
+		$this->info('Downloading OPSkins information from CDN...');
+		\Log::info('Downloading OPSkins information from CDN...');
+		$inventory = Curl::to('https://api.opskins.com/IPricing/GetPriceList/v2/?appid=730')->asJson()->get();
+		$this->info('Received information from CDN!');
+		\Log::info('Received information from CDN!');
 
-        if (!isset($inventory->response)) {
-            $this->error('Invalid response from OPSkins, quitting before truncating database');
-            \Log::error('Invalid response from OPSkins, quitting before truncating database');
+		if (!isset($inventory->response)) {
+			$this->error('Invalid response from OPSkins, quitting before truncating database');
+			\Log::error('Invalid response from OPSkins, quitting before truncating database');
 
-            return;
-        }
+			return;
+		}
 
-        $size = count((array) $inventory->response);
-        $this->info('Received '.$size.' items from OPSkins.');
-        \Log::info('Received '.$size.' items from OPSkins.');
+		$size = count((array)$inventory->response);
+		$this->info('Received ' . $size . ' items from OPSkins.');
+		\Log::info('Received ' . $size . ' items from OPSkins.');
 
-        $index = 1;
-        $oldPercent = 0;
+		$index = 1;
+		$oldPercent = 0;
 
-        $this->info('Truncating database.');
-        \Log::info('Truncating database.');
-        OPSkinsCache::truncate();
+		$this->info('Truncating database.');
+		\Log::info('Truncating database.');
+		OPSkinsCache::truncate();
 
-        $now = Carbon::now();
+		$now = Carbon::now();
 
-        foreach ($inventory->response as $key => $value) {
-            $perCent = round($index++ / $size * 10);
-            if ($perCent != $oldPercent) {
-                // $this->info('Sending [' . $index++ . '/' . $size . '] items to database.');
-                $this->info('Sent '.($perCent * 10).'% items to database.');
-                \Log::info('Sent '.($perCent * 10).'% items to database.');
-                $oldPercent = $perCent;
-            }
-            $name = $key;
-            $meanSum = 0;
-            $sumCount = 0;
+		$added = 0;
 
-            foreach ($value as $k=>$v) {
-                $maxDate = Carbon::createFromFormat('Y-m-d', $k);
+		foreach ($inventory->response as $key => $value) {
+			$perCent = round($index++ / $size * 10);
+			if ($perCent != $oldPercent) {
+				// $this->info('Sending [' . $index++ . '/' . $size . '] items to database.');
+				$this->info('Sent ' . ($perCent * 10) . '% items to database.');
+				\Log::info('Sent ' . ($perCent * 10) . '% items to database.');
+				$oldPercent = $perCent;
+			}
+			$name = $key;
+			$meanSum = 0;
+			$sumCount = 0;
 
-                if ($maxDate->diffInDays($now) > 7) {
-                    continue;
-                }
+			foreach ($value as $k => $v) {
+				$maxDate = Carbon::createFromFormat('Y-m-d', $k);
 
-                $sumCount++;
-                $meanSum += $v->normalized_mean;
-            }
+				if ($maxDate->diffInDays($now) > 7) {
+					continue;
+				}
 
-            if ($sumCount >= 7) {
-                try {
-                    OPSkinsCache::create([
-                        'name'  => $name,
-                        'price' => $meanSum / $sumCount,
-                    ]);
-                } catch (\Exception $e) {
-                    $this->warn('Error: '.$e->getMessage());
-                    \Log::warning('Error: '.$e->getMessage());
-                    continue;
-                }
-                if ($this->option('detailed')) {
-                    $this->info($name.' added to database = '.$meanSum.' / '.$sumCount.' = '.($meanSum / $sumCount));
-                    \Log::info($name.' added to database = '.$meanSum.' / '.$sumCount.' = '.($meanSum / $sumCount));
-                }
-            } else {
-                if ($this->option('detailed')) {
-                    $this->warn($name.' returned < 7 counts');
-                    \Log::warning($name.' returned < 7 counts');
-                }
-            }
-        }
+				$sumCount++;
+				$meanSum += $v->normalized_mean;
+			}
 
-        $this->info('OPSkins cache refreshed!');
-    }
+			if ($sumCount >= 7) {
+				try {
+					OPSkinsCache::create([
+						'name'  => $name,
+						'price' => $meanSum / $sumCount,
+					]);
+
+					$added++;
+				} catch (\Exception $e) {
+					$this->warn('Error: ' . $e->getMessage());
+					\Log::warning('Error: ' . $e->getMessage());
+					continue;
+				}
+				if ($this->option('detailed')) {
+					$this->info($name . ' added to database = ' . $meanSum . ' / ' . $sumCount . ' = ' . ($meanSum / $sumCount));
+					\Log::info($name . ' added to database = ' . $meanSum . ' / ' . $sumCount . ' = ' . ($meanSum / $sumCount));
+				}
+			} else {
+				if ($this->option('detailed')) {
+					$this->warn($name . ' returned < 7 counts');
+					\Log::warning($name . ' returned < 7 counts');
+				}
+			}
+		}
+
+		$this->info('OPSkins cache refreshed! Added total of ' . $added	. ' items to database.');
+		\Log::info('OPSkins cache refreshed! Added total of ' . $added	. ' items to database.');
+	}
 }
