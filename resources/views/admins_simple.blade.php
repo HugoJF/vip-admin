93514a4e8d0f182d0e97009d5b00f761b4da53aa
Hugo Jeller <hugo_jeller@hotmail.com>
2018-01-24 16:58:50 -0300

    Added permanent vips

--- resources/views/admins_simple.blade.php
+++ resources/views/admins_simple.blade.php
@@ -59,6 +59,13 @@
 "STEAM_1:0:24572716"		"50:a" // delboNi
 "STEAM_1:0:89523495"		"50:a" // land1n
 
+// SHARKS
+"STEAM_0:0:25115307"		"50:a" // exitt
+"STEAM_0:1:20413271"		"50:a" // ghtw
+"STEAM_0:1:21663009"		"50:a" // jntcsgo
+"STEAM_0:1:91300"			"50:a" // RNakano10
+"STEAM_0:1:29145638"		"50:a" // leo_druNky
+
 // CAPS
 "STEAM_1:1:243947646"	"50:a" // No0box2			| Janeiro e Fevereiro
 
@@ -66,6 +73,7 @@
 "STEAM_0:1:176486595"		"50:a" // HUAL.2			| R$ 10,00 - #3254351134
 "STEAM_1:1:25030091"		"50:a" // HuaL				| R$ 10,00 - #3254351134
 "STEAM_0:0:50794943"		"50:a" // NuneS				| R$ 4,00 - #3313430344
+"STEAM_0:1:58655438"		"50"a  // AdM 				| R$ 4,00 - #3346841365
 
 // VIPS MERCADO PAGO
 "STEAM_0:0:234743042"		"50:a" // Infel1z			| Glock-18 | Water Elemental (Field-Tested)
