92a8bc2381765929a856366d7964d7e846cdb3a9
Hugo Jeller <hugojellerferreira@hotmail.com>
2018-01-24 19:51:20 +0000

    Apply fixes from StyleCI

--- app/Confirmation.php
+++ app/Confirmation.php
@@ -10,81 +10,81 @@ use Illuminate\Support\Facades\View;
 
 class Confirmation extends Model
 {
-	protected $dates = [
-		'start_period',
-		'end_period',
-		'created_at',
-		'updated_at',
-	];
+    protected $dates = [
+        'start_period',
+        'end_period',
+        'created_at',
+        'updated_at',
+    ];
 
-	public function baseOrder()
-	{
-		return $this->belongsTo('App\Order', 'order_id');
-	}
+    public function baseOrder()
+    {
+        return $this->belongsTo('App\Order', 'order_id');
+    }
 
-	public function user()
-	{
-		return $this->belongsTo('App\User');
-	}
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 
-	public function scopeValid($query)
-	{
-		$now = Carbon::now();
+    public function scopeValid($query)
+    {
+        $now = Carbon::now();
 
-		return $query->where([
-			['start_period', '<=', $now],
-			['end_period', '>=', $now],
-		]);
-	}
+        return $query->where([
+            ['start_period', '<=', $now],
+            ['end_period', '>=', $now],
+        ]);
+    }
 
-	public static function syncServer()
-	{
-		try {
-			DaemonController::consoleLog('Generating_new_admins_simple');
+    public static function syncServer()
+    {
+        try {
+            DaemonController::consoleLog('Generating_new_admins_simple');
 
-			$confirmations = self::valid()->with('baseOrder.user', 'baseOrder')->get();
+            $confirmations = self::valid()->with('baseOrder.user', 'baseOrder')->get();
 
-			$steamid = [];
+            $steamid = [];
 
-			foreach ($confirmations as $confirmation) {
-				$steam2 = DaemonController::getSteam2ID($confirmation->baseOrder->user->steamid);
-				$steamid[] = [
-					'id'           => $steam2,
-					'confirmation' => $confirmation,
-				];
+            foreach ($confirmations as $confirmation) {
+                $steam2 = DaemonController::getSteam2ID($confirmation->baseOrder->user->steamid);
+                $steamid[] = [
+                    'id'           => $steam2,
+                    'confirmation' => $confirmation,
+                ];
 
-				$confirmation->baseOrder->server_uploaded = true;
-				$saved = $confirmation->baseOrder->save();
-				if (!$saved) {
-					flash()->error('Error saving confirmation details.');
+                $confirmation->baseOrder->server_uploaded = true;
+                $saved = $confirmation->baseOrder->save();
+                if (!$saved) {
+                    flash()->error('Error saving confirmation details.');
 
-					return redirect()->route('home');
-				}
-			}
+                    return redirect()->route('home');
+                }
+            }
 
-			$view = View::make('admins_simple', [
-				'list' => $steamid,
-				'html' => false,
-			]);
+            $view = View::make('admins_simple', [
+                'list' => $steamid,
+                'html' => false,
+            ]);
 
-			if (config('app.update_server') == 'true') {
-				Storage::put('admins_simple.ini', $view);
+            if (config('app.update_server') == 'true') {
+                Storage::put('admins_simple.ini', $view);
 
-				DaemonController::updateSourceMod();
-			}
-		} catch (Exception $e) {
-			flash()->error("Error syncing server: " . $e->getMessage());
+                DaemonController::updateSourceMod();
+            }
+        } catch (Exception $e) {
+            flash()->error('Error syncing server: '.$e->getMessage());
 
-			return redirect()->route('home');
-		}
+            return redirect()->route('home');
+        }
 
-		return true;
-	}
+        return true;
+    }
 
-	public function isValid()
-	{
-		$now = Carbon::now();
+    public function isValid()
+    {
+        $now = Carbon::now();
 
-		return $this->start_period <= $now && $this->end_period >= $now;
-	}
+        return $this->start_period <= $now && $this->end_period >= $now;
+    }
 }
