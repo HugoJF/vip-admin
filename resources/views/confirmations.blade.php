a7b4dc38a1a39c32eba8fcaaf0fcc9acfef33b10
Hugo Jeller <hugo_jeller@hotmail.com>
2018-01-24 16:24:07 -0300

    added public_id and user name

--- resources/views/confirmations.blade.php
+++ resources/views/confirmations.blade.php
@@ -6,7 +6,9 @@
     <table class="table table-striped">
         <thead>
         <tr>
+            <th>Confirmation Public ID</th>
             <th>Order Public ID</th>
+            <th>Username</th>
             <th>Starting Period</th>
             <th>Ending Period</th>
             <th>State</th>
@@ -16,7 +18,11 @@
         <tbody>
         @foreach($confirmations as $confirmation)
             <tr>
+                <td>{{ $confirmation->public_id }}</td>
                 <td scope="row"><a href="{{ route('view-steam-order', $confirmation->baseOrder->public_id) }}">#{{ $confirmation->baseOrder->public_id }}</a></td>
+                @if($isAdmin)
+                    <td><a href="http://steamcommunity.com/profiles/{{ $confirmation->user->steamid }}">{{ $confirmation->user->username }}</a></td>
+                @endif
                 <td>{{ $confirmation->start_period }}</td>
                 <td>{{ $confirmation->end_period }}</td>
                 <td><span class="label label-{{ $confirmation->isValid() ? 'success' : 'danger' }}"> {{ $confirmation->isValid() ? 'Valid' : 'Expired' }}</span></td>
