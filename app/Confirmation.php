7cccf4dd48887f44c91e39e476c367b96166eb24
Hugo Jeller <hugo_jeller@hotmail.com>
2018-01-24 16:47:46 -0300

    even more ragE

--- app/Confirmation.php
+++ app/Confirmation.php
@@ -3,7 +3,6 @@
 namespace App;
 
 use App\Http\Controllers\DaemonController;
-use App\Http\Controllers\DaemonController;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Storage;
