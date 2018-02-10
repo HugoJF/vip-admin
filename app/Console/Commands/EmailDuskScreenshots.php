77d4e6294d07ae148f49e4f00dc995750a2df2e4
Hugo Jeller <hugo_jeller@hotmail.com>
2018-02-10 20:36:55 -0300

    Added path helpers

--- app/Console/Commands/EmailDuskScreenshots.php
+++ app/Console/Commands/EmailDuskScreenshots.php
@@ -8,41 +8,41 @@ use Illuminate\Support\Facades\File;
 
 class EmailDuskScreenshots extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'dusk:email-screenshots';
+	/**
+	 * The name and signature of the console command.
+	 *
+	 * @var string
+	 */
+	protected $signature = 'dusk:email-screenshots';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Emails every error screenshot taken during testing';
+	/**
+	 * The console command description.
+	 *
+	 * @var string
+	 */
+	protected $description = 'Emails every error screenshot taken during testing';
 
-    /**
-     * Create a new command instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        parent::__construct();
-    }
+	/**
+	 * Create a new command instance.
+	 *
+	 * @return void
+	 */
+	public function __construct()
+	{
+		parent::__construct();
+	}
 
-    /**
-     * Execute the console command.
-     *
-     * @return mixed
-     */
-    public function handle()
-    {
-        $directory = __DIR__.'/../../../tests/Browser/screenshots/';
+	/**
+	 * Execute the console command.
+	 *
+	 * @return mixed
+	 */
+	public function handle()
+	{
+		$directory = __DIR__ . base_path('tests/Browser/screenshots/');
 
-        $files = File::allFiles($directory);
+		$files = File::allFiles($directory);
 
-        \Mail::to('hugo_jeller@hotmail.com')->send(new DuskScreenshotMail($files));
-    }
+		\Mail::to('hugo_jeller@hotmail.com')->send(new DuskScreenshotMail($files));
+	}
 }
