c275f530ba57ab31b7eae5eadea882d9399fb7c1
Hugo Jeller <hugo_jeller@hotmail.com>
2018-02-12 04:29:54 -0300

    Only send emails on production

--- app/Listeners/NotifyAdmins.php
+++ app/Listeners/NotifyAdmins.php
@@ -28,7 +28,7 @@ class NotifyAdmins
      */
     public function handle(IMailableEvent $event)
     {
-        if ($event->user()) {
+        if ($event->user() && App::environment('production')) {
             Mail::to('hugo_jeller@hotmail.com')->send(new AdminMessageMail($event));
         }
     }
