7967b3691ef5ca2ae7188ed1c2ee6ab5fffca43c
Hugo Jeller <hugojellerferreira@hotmail.com>
2018-02-13 22:31:33 +0000

    Apply fixes from StyleCI

--- tests/Browser/Pages/Home.php
+++ tests/Browser/Pages/Home.php
@@ -6,59 +6,59 @@ use Laravel\Dusk\Browser;
 
 class Home extends Page
 {
-	/**
-	 * Get the URL for the page.
-	 *
-	 * @return string
-	 */
-	public function url()
-	{
-		return '/';
-	}
+    /**
+     * Get the URL for the page.
+     *
+     * @return string
+     */
+    public function url()
+    {
+        return '/';
+    }
 
-	/**
-	 * Assert that the browser is on the page.
-	 *
-	 * @param Browser $browser
-	 *
-	 * @return void
-	 */
-	public function assert(Browser $browser)
-	{
-		$browser->assertPathIs($this->url())
-				->assertSee('Bem vindo');
-	}
+    /**
+     * Assert that the browser is on the page.
+     *
+     * @param Browser $browser
+     *
+     * @return void
+     */
+    public function assert(Browser $browser)
+    {
+        $browser->assertPathIs($this->url())
+                ->assertSee('Bem vindo');
+    }
 
-	/**
-	 * Get the element shortcuts for the page.
-	 *
-	 * @return array
-	 */
-	public function elements()
-	{
-		return [
-			'@home'			   => '#home',
+    /**
+     * Get the element shortcuts for the page.
+     *
+     * @return array
+     */
+    public function elements()
+    {
+        return [
+            '@home'			   => '#home',
 
-			'@buy-with-skins'  => '#buy-with-skins',
-			'@buy-with-tokens' => '#buy-with-tokens',
-			'@buy-with-mp'     => '#buy-with-mp',
+            '@buy-with-skins'  => '#buy-with-skins',
+            '@buy-with-tokens' => '#buy-with-tokens',
+            '@buy-with-mp'     => '#buy-with-mp',
 
-			'@logs'            => '#daemon-logs',
-			'@stdout'          => '#daemon-stdout',
-			'@stderr'          => '#daemon-stderr',
+            '@logs'            => '#daemon-logs',
+            '@stdout'          => '#daemon-stdout',
+            '@stderr'          => '#daemon-stderr',
 
-			'@orders'          => '#orders',
-			'@confirmations'   => '#confirmations',
-			'@tokens'          => '#tokens',
-			'@settings'        => '#settings',
+            '@orders'          => '#orders',
+            '@confirmations'   => '#confirmations',
+            '@tokens'          => '#tokens',
+            '@settings'        => '#settings',
 
-			'@generate-tokens' => '#generate-tokens',
-			'@admins-simple'   => '#admins-simple-preview',
-			'@users'           => '#users',
-			'@laravel-logs'    => '#laravel-logs',
-			'@server-list'     => '#server-list',
-			'@opskins-updater' => '#opskins-updater',
-			'@app-settings'    => '#app-settings',
-		];
-	}
+            '@generate-tokens' => '#generate-tokens',
+            '@admins-simple'   => '#admins-simple-preview',
+            '@users'           => '#users',
+            '@laravel-logs'    => '#laravel-logs',
+            '@server-list'     => '#server-list',
+            '@opskins-updater' => '#opskins-updater',
+            '@app-settings'    => '#app-settings',
+        ];
+    }
 }
