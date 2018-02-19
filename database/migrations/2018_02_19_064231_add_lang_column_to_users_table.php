e66ab138b63e158cf060dd12b7f6f2d9febd49b4
Hugo Jeller <hugojellerferreira@hotmail.com>
2018-02-19 07:09:13 +0000

    Apply fixes from StyleCI

--- database/migrations/2018_02_19_064231_add_lang_column_to_users_table.php
+++ database/migrations/2018_02_19_064231_add_lang_column_to_users_table.php
@@ -1,8 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Schema;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
 class AddLangColumnToUsersTable extends Migration
 {
@@ -26,7 +26,7 @@ class AddLangColumnToUsersTable extends Migration
     public function down()
     {
         Schema::table('users', function (Blueprint $table) {
-        	$table->dropColumn('lang');
+            $table->dropColumn('lang');
         });
     }
 }
