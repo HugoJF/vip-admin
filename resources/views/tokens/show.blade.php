ba4fad7a9a3a9bf623057dde7c1beb75c6e4a39f
Hugo Jeller <hugo_jeller@hotmail.com>
2019-01-26 07:30:28 -0300

    fixed users settings link and renamed confirmation to activation

--- resources/views/tokens/show.blade.php
+++ resources/views/tokens/show.blade.php
@@ -2,36 +2,36 @@
 
 @section('content')
     
-    <h2>Viewing token: <strong>{{ $token->token }}</strong></h2>
+    <h2>@lang('messages.viewing-token'): <strong>{{ $token->token }}</strong></h2>
     
     <table class="table table-hover">
         <tbody>
         <tr>
-            <td>Token</td>
+            <td>@lang('messages.token')</td>
             <td><span class="label label-success">{{ $token->token}}</span></td>
         </tr>
         <tr>
-            <td>Duration</td>
-            <td><span class="label label-success">{{ $token->duration }} days</span></td>
+            <td>@lang('messages.duration')</td>
+            <td><span class="label label-success">{{ $token->duration }} @lang('messages.time.days', $token->duration)</span></td>
         </tr>
         <tr>
-            <td>Expiration</td>
-            <td><span class="label label-success">{{ $token->expiration }} hours</span></td>
+            <td>@lang('messages.expiration')</td>
+            <td><span class="label label-success">{{ $token->expiration }} @lang('messages.time.hours', $token->expiration)</span></td>
         </tr>
         <tr>
-            <td>Expiration Date</td>
+            <td>@lang('messages.expiration-date')</td>
             <td><span class="label label-success">{{ \Carbon\Carbon::now()->addHours($token->expiration) }}</span></td>
         </tr>
         <tr>
-            <td>Expiration Remaining</td>
+            <td>@lang('messages.expiration-remaining')</td>
             <td><span class="label label-success">{{ \Carbon\Carbon::now()->addHours($token->expiration)->diffForHumans() }}</span></td>
         </tr>
         <tr>
-            <td>Note</td>
+            <td>@lang('messages.note')</td>
             <td><span class="label label-success">{{ $token->note }}</span></td>
         </tr>
         <tr>
-            <td>Status</td>
+            <td>@lang('messages.status')</td>
             <td><span class="label label-{{ $token->status()['class'] }}">{{ $token->status()['text'] }}</span></td>
         </tr>
         </tbody>
@@ -39,7 +39,7 @@
     
     <p>
         <a class="btn btn-primary btn-block" href="{{ route('token-orders.create', ['token' => $token->token]) }}">Redeem Link</a>
-        <a class="btn btn-success btn-lg btn-block clipboard-js" data-clipboard-text="{{ route('tokens.show', $token->token) }}">Copy Link</a>
+        <a class="btn btn-success btn-lg btn-block clipboard-js" data-clipboard-text="{{ route('token-orders.create', $token->token) }}">Copy Link</a>
     </p>
 
 @endsection
